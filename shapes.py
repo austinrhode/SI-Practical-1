d53dc8a93921423a51a2876b137b6e33e1044950
Austin Rhode <ajr3021@g.rit.edu>
2021-02-14 12:39:53 -0500

    Practical code added

---
+++ shapes.py
@@ -0,0 +1,40 @@
+"""
+Write a program that prompts the user to enter three things,
+the number of sides a shape has,
+the length of those sides,
+and the number of shapes that they want to draw.
+
+Once you have this information, your program should draw regular polygons
+with the number of sides indicated with the correct length.
+After drawing each polygon, it will rotate and draw the next, such that all
+the polygons drawn are in a circle (aka they an equal number of degrees apart out of 360 degrees in a circle)
+
+After drawing each shape, you program should switch pen colors
+cycling between three colors of your choice.
+So the first shape could be blue, then the second red, then the third green, then the fourth blue, and so on.
+
+
+You program should also use TDD. I can't make you do this, but they will on the real exam.
+"""
+def choice_color(number):
+    pass
+
+
+def init():
+    pass
+
+
+
+def draw_any_shape(num_sizes, size, color):
+    pass
+
+
+def draw_larger_shape(number_of_sides, size, number_of_shapes):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
